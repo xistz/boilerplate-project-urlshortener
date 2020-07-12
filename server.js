42fb1c43da2397b644bb44391844508b4b521dfe
Jonathan Chan <tmxistz@gmail.com>
2020-07-12 13:52:29 +0900

    added mongoose and healthcheck routes

--- server.js
+++ server.js
@@ -8,11 +8,15 @@ var cors = require('cors');
 
 var app = express();
 
-// Basic Configuration 
+// Basic Configuration
 var port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
-// mongoose.connect(process.env.DB_URI);
+/** this project needs a db !! **/
+
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(cors());
 
@@ -21,16 +25,24 @@ app.use(cors());
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/health', (req, res) => {
+  res.json({ status: 'healthy' });
+});
+
+app.get('/api/ready', (req, res) => {
+  const ready = mongoose.connection.readyState;
+
+  res.json({ status: ready ? 'ready' : 'not ready' });
+});
 
 app.listen(port, function () {
   console.log('Node.js listening ...');
